9eede57debe85424110f348ed16c85353f7eb37f
Abdelbasset10 <basset.rezazi26@gmail.com>
2023-11-09 14:40:22 +0100

    resolver express deploy error

--- server/index.js
+++ server/index.js
@@ -4,19 +4,25 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 
-const corsOptions = require("../config/corsConfig");
+const corsOptions = require("./config/corsConfig");
 
-const prisma = require("../utils/prisma");
+const prisma = require("./utils/prisma");
 
-const authRouter = require("../routes/auth");
-const refreshRouter = require("../routes/refresh");
-const courseRouter = require("../routes/course");
-const categoryRouter = require("../routes/category");
-const chapterRouter = require("../routes/chapter");
+const authRouter = require("./routes/auth");
+const refreshRouter = require("./routes/refresh");
+const courseRouter = require("./routes/course");
+const categoryRouter = require("./routes/category");
+const chapterRouter = require("./routes/chapter");
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+const corsConfig = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
 app.use(cookieParser());
 app.use(express.json());
 
